5bbe052c8b2e46093258c3f07c720a31418ff455
Alain Chea <alain.chea@efrei.net>
2024-07-15 23:37:25 +0200

    refactor(components) : button

--- src/app/components/MovieCard/MovieCard.tsx
+++ src/app/components/MovieCard/MovieCard.tsx
@@ -1,23 +1,23 @@
 "use client";
-import React, { use } from "react";
+import React from "react";
 import styles from "./MovieCard.module.scss";
 import Image from "next/image";
-import StarSvg from "@/app/assets/vectors/star.svg";
 import { useRouter } from "next/navigation";
+
 type Props = {
   image: string;
-  title: string;
-  releaseDate: string;
+  title?: string;
+  releaseDate?: string;
   id: number;
 };
 
 const MovieCard = ({ image, title, releaseDate, id }: Props) => {
   const router = useRouter();
   const handleClick = () => {
-    router.push(`/movies/${id}`);
+    router.push(`/movies/${title}?id=${id}`);
   };
   return (
-    <div className={styles.container} onClick={handleClick}>
+    <button className={styles.container} onClick={handleClick}>
       <Image
         src={`https://image.tmdb.org/t/p/w500${image}`}
         alt="movie title"
@@ -27,9 +27,9 @@ const MovieCard = ({ image, title, releaseDate, id }: Props) => {
       />
       <div className={styles.infoContainer}>
         <h2 className={styles.title}>{title} </h2>
-        <span className={styles.infoText}>released {releaseDate}</span>
+        {/* <span className={styles.infoText}> {releaseDate}</span> */}
       </div>
-    </div>
+    </button>
   );
 };
 
