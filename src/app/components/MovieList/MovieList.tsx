2b005367e42de6e8297ac0ed928b21488402a070
Alain Chea <alain.chea@efrei.net>
2024-07-17 01:48:25 +0200

    fix(movieList): center issue

--- src/app/components/MovieList/MovieList.tsx
+++ src/app/components/MovieList/MovieList.tsx
@@ -4,6 +4,7 @@ import styles from "./MovieList.module.scss";
 import useMovies from "@/app/hooks/useMovies";
 import { Movie } from "@/../global";
 import MovieCard from "@/app/components/MovieCard/MovieCard";
+import cn from "classnames";
 
 type Props = {
   searchValue: string;
@@ -11,25 +12,28 @@ type Props = {
 
 const MovieList = ({ searchValue }: Props) => {
   const { movies, isLoading, error } = useMovies();
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading data.</p>;
+  const movieListFiltered = movies?.filter((movie: Movie) =>
+    movie.title.toLowerCase().includes(searchValue ?? "")
+  );
+  const maxMovieInaRow = 4;
 
   return (
-    <div className={styles.movieList}>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-
-      {movies
-        ?.filter((movie: Movie) =>
-          movie.title.toLowerCase().includes(searchValue ?? "")
-        )
-        ?.map((movie: Movie) => (
-          <MovieCard
-            key={movie.id}
-            image={movie.backdrop_path}
-            title={movie.title}
-            releaseDate={movie.release_date}
-            id={movie.id}
-          />
-        ))}
+    <div
+      className={cn(styles.movieList, {
+        [styles.center]: movieListFiltered.length > maxMovieInaRow,
+      })}
+    >
+      {movieListFiltered?.map((movie: Movie) => (
+        <MovieCard
+          key={movie.id}
+          image={movie.backdrop_path}
+          title={movie.title}
+          releaseDate={movie.release_date}
+          id={movie.id}
+        />
+      ))}
     </div>
   );
 };
